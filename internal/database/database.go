8359c27acad811a95ce5860dd378fecf31b186fa
artyom-kalman <p.a.kalashnikov@ya.ru>
2025-03-18 21:37:50 +0900

    Add reties connection to DB

--- internal/database/database.go
+++ internal/database/database.go
@@ -3,11 +3,17 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
 	"github.com/artyom-kalman/simple-registration-youteam/configs"
 	"github.com/artyom-kalman/simple-registration-youteam/pkg/logger"
 
 	_ "github.com/lib/pq"
+)
+
+var (
+	maxRetries = 5
+	retryDelay = 5 * time.Second
 )
 
 type Database struct {
@@ -22,9 +28,16 @@ func NewDatabase(config *configs.DBConfig) (*Database, error) {
 	)
 	logger.Debug("Connection string: %s", connString)
 
-	db, err := sql.Open("postgres", connString)
-	if err != nil {
-		return nil, fmt.Errorf("error connection to database: %v", err)
+	var db *sql.DB
+	var err error
+	for range maxRetries {
+		db, err = sql.Open("postgres", connString)
+		if err = db.Ping(); err != nil {
+			logger.Info("Failed to connect: %v. Retrying...", err)
+			time.Sleep(retryDelay)
+			continue
+		}
+		break
 	}
 
 	err = db.Ping()
